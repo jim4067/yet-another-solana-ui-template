3236833a7e01407c8057fde46aea14f295122a97
Jimii <jimmyimpulse2@gmail.com>
2024-10-07 20:44:11 +0300

    fix:failing fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,5 @@
 import {
-	fetchAllDigitalAssetWithTokenByOwner,
+	DigitalAsset,
 	fetchDigitalAssetWithAssociatedToken,
 } from "@metaplex-foundation/mpl-token-metadata";
 import {
@@ -9,40 +9,12 @@ import {
 } from "@metaplex-foundation/umi";
 import { PublicKey } from "@solana/web3.js";
 import axios from "axios";
-import { MarketplaceAsset } from "./definations";
-
-export async function fetchUserAssets(
-	umiInstance: Umi,
-	owner: PublicKey
-): Promise<MarketplaceAsset[]> {
-	const digitalAssets = await fetchAllDigitalAssetWithTokenByOwner(
-		umiInstance,
-		UmiPublicKey(owner)
-	);
-
-	// filter out collections
-	const resp = await Promise.all(
-		digitalAssets
-			.filter((asset) => isSome(asset.metadata.collection))
-			.map(async (asset) => {
-				const offChainData = (await axios.get(asset.metadata.uri)).data;
-				return {
-					image: offChainData.image,
-					name: offChainData.name,
-					mint: asset.mint,
-					offChainData,
-				};
-			})
-	);
-
-	return resp;
-}
 
 export async function fetchAssetByMint(
 	umiInstance: Umi,
 	mint: string,
 	owner: PublicKey
-): Promise<MarketplaceAsset> {
+): Promise<DigitalAsset & { name: string; image: string }> {
 	const resp = await fetchDigitalAssetWithAssociatedToken(
 		umiInstance,
 		UmiPublicKey(mint),
